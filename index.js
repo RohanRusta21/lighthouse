3e800016e350eaaab58294080e727f77c4488bb0
Rohan Maharjan <rohan.maharjan@harness.io>
2023-07-28 07:57:17 +0545

    first

---
+++ index.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+import lighthouse from "lighthouse";
+import puppeteer from "puppeteer";
+import chalk from "chalk";
+
+//REPORT//
+function createBigText(text) {
+  const characters = {
+    R: ["█████ ", "█   ██", "█████ ", "█   ██", "█    █"],
+    E: ["██████", "█     ", "█████ ", "█     ", "██████"],
+    P: ["█████ ", "█   ██", "█████ ", "█     ", "█     "],
+    O: ["█████", "█   █", "█   █", "█   █", "█████"],
+    T: ["██████", "  ██  ", "  ██  ", "  ██  ", "  ██  "],
+  };
+  const rows = Array.from({ length: 5 }, () => "");
+
+  for (const char of text) {
+    const charData = characters[char.toUpperCase()] || characters[" "];
+
+    for (let i = 0; i < 5; i++) {
+      rows[i] += charData[i] + "  ";
+    }
+  }
+
+  const bigText = rows.map((row) => chalk.blue(row)).join("\n");
+  return bigText;
+}
+const reportText = "REPORT";
+const bigReportText = createBigText(reportText);
+
+//RUN AUDITS//
+const RunAudits = async (url) => {
+  let result;
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-gpu"],
+  });
+  const browserWSEndpoint = browser.wsEndpoint();
+  const { port } = new URL(browserWSEndpoint);
+  try {
+    result = await GenerateReport(url, port);
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    await browser.close();
+    console.log(bigReportText);
+    console.log(chalk.bold.blue("Given URL : ") + chalk.bold.green(result.url));
+    console.log(
+      chalk.bold.yellowBright("performance : ") +
+        chalk.magentaBright(result.result.performance)
+    );
+    console.log(
+      chalk.bold.yellowBright("bestpractices : ") +
+        chalk.magentaBright(result.result.bestpractices)
+    );
+    console.log(
+      chalk.bold.yellowBright("accessibility : ") +
+        chalk.magentaBright(result.result.accessibility)
+    );
+    console.log(
+      chalk.bold.yellowBright("seo : ") + chalk.magentaBright(result.result.seo)
+    );
+
+    return result;
+  }
+};
+//GENERATE REPORT//
+const GenerateReport = async (url, port) => {
+  const options = {
+    logLevel: "info",
+    onlyCategories: ["performance", "accessibility", "best-practices", "seo"],
+    audits: [
+      "first-meaningful-paint",
+      "first-cpu-idle",
+      "byte-efficiency/uses-optimized-images",
+    ],
+    port: parseInt(port),
+    strategy: "desktop",
+    timeoutMs: 60000,
+  };
+
+  const runnerResult = await lighthouse(url, options);
+
+  const result_object = {
+    performance: runnerResult.lhr.categories.performance?.score * 100 || "NA",
+    accessibility:
+      runnerResult.lhr.categories.accessibility?.score * 100 || "NA",
+    bestpractices:
+      runnerResult.lhr.categories["best-practices"]?.score * 100 || "NA",
+    seo: runnerResult.lhr.categories.seo?.score * 100 || "NA",
+  };
+
+  const page = { url, result: result_object };
+  return page;
+};
+
+program
+  .option(
+    "-u, --url <url of site to generate lighthouse report>",
+    "Please enter a url"
+  )
+  .action((cmd) => {
+    return RunAudits(cmd.url);
+  });
+
+program.parse(process.argv);
---
+++ readme.md
@@ -0,0 +1,41 @@
+
+
+# To deploy lighthouse in minikube
+
+Step 1 : Build docker image and push image to docker hub
+
+
+```shell
+docker build -t rohanmaharjan100/lighthouse .
+docker image push rohanmaharjan100/lighthouse:latest
+docker run -dit  rohanmaharjan100/lighthouse
+docker exec f66bde16b4841d66310ea24b60bd5355163a4559554c59d33828881a2cc5e3e5 npx get --url https://facebook.com
+```
+
+Step 2: Start minikube with command :
+
+```shell
+minikube start
+```
+
+Step 3: Create deployment using deployment command :
+
+```shell
+kubectl apply -f Deployment.yaml
+```
+
+check deployments using commands
+
+```shell
+kubectl get deployments
+minikube dashboard
+
+```
+
+Step 4:Expose the pods running within the cluster to the outside world to be accessible:
+
+```shell
+kubectl expose deployment lighthouse-deployment --port=4000
+kubectl port-forward service/lighthouse-deployment   4000:4000
+
+```
